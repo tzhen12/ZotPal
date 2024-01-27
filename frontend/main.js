6e629c5c4fa17079c05afd8e230059451bc8d97e
trantripaul <trantripaul@gmail.com>
2024-01-26 23:33:19 -0800

    start :D

---
+++ frontend/main.js
@@ -0,0 +1,7 @@
+
+const viewCLassesText = document.querySelector('.view-class');
+const viewClasses = document.querySelector("example");
+const btn = document.querySelector('.btn');
+//btn.addEventListener('click', function()) {
+//    viewClass.classList.toggle('example');
+//}
