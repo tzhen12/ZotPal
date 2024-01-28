a09eecd7243c441462ce4b1b170f9edb4bf2f770
trantripaul <trantripaul@gmail.com>
2024-01-27 16:22:48 -0800

    add level up

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,16 @@
 let level = 1;
 const button = document.getElementById('level-up');
 const display = document.getElementById('level'); 
+const image = document.getElementById('anteater');
 
 button.addEventListener('click', levelUp);
 
 function levelUp() {
     level++;
     display.innerHTML = level;
+
+    if(level >=10){
+        image.src = "https://media.tenor.com/2l4-h42qnmcAAAAi/toothless-dancing-toothless.gif"
+    }
     }
 
